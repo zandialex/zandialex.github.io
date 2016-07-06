1b05c622872ed9be655e741b17272a8b9cc0019c
zandialex <zandialex@gmail.com>
2016-07-05 21:54:47 -0400

    .

--- js/index.js
+++ js/index.js
@@ -1,13 +1,38 @@
+
+var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+
+// nav blur
+if (isSafari) {
+  $("#nav").css("background-color", "rgba(249,246,243,0.75)");
+  $("#nav").css("-webkit-backdrop-filter", "blur(20px)");
+  $("#nav").css("backdrop-filter", "blur(20px)");
+};
+
+
+
+
+
 $(window).scroll(function(){
   var scrollPos = $(this).scrollTop();
   var bannerPos = $("#banner").offset().top;
 
-  if (scrollPos > 2) {
-    $("#nav").css("box-shadow", "0px 1px 6px 0px rgba(0,0,0,0.08)");
-    console.log("activated");
+  if (!isSafari) {
+    if (scrollPos > 2) {
+      $("#nav").css("box-shadow", "0px 1px 6px 0px rgba(0,0,0,0.08)");
+    }
+    else {
+      $("#nav").css("box-shadow", "0px 1px 6px 0px rgba(0,0,0,0.0)");
+    }
   }
+
+  // safari
   else {
-    $("#nav").css("box-shadow", "0px 1px 6px 0px rgba(0,0,0,0.0)");
+    if (scrollPos > 2) {
+      $("#nav").css("box-shadow", "0px 1px 0px 0px rgba(0,0,0,0.07)");
+    }
+    else {
+      $("#nav").css("box-shadow", "0px 1px 6px 0px rgba(0,0,0,0.0)");
+    }
   }
 });
 
